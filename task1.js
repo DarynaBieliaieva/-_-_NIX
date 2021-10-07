66a3dd2f53e903986d6142afc9c59bf8f4e18804
Daryna Bieliaieva <85941764+DarynaBieliaieva@users.noreply.github.com>
2021-10-07 15:32:50 +0300

    Add files via upload

---
+++ task1.js
@@ -0,0 +1,36 @@
+/* 
+Практика JS 1.
+Создайте переменную с помощью camelCase, snake_case и PascalCase и присвойте им значения от 0-я до 2-х.
+ Выведите значения этих переменных на экран с помощью метода alert и console.log. Скажите, чем кардинально отличается alert и console.log?
+*/
+
+// camelCase
+
+let userLoginCount = 0;
+alert(userLoginCount);
+
+let userLoginCount = 0;
+console.log(userLoginCount);
+
+// PascalCase
+
+let UserLoginCount = 1;
+alert(UserLoginCount);
+
+let UserLoginCount = 1;
+console.log(UserLoginCount);
+
+// snake_case
+
+let user_login_count = 2;
+alert(user_login_count);
+
+
+let user_login_count = 2;
+console.log(user_login_count);
+
+/*
+В JavaScript функция alert позволяет вывести какой-либо текст в окно браузера в виде диалогового окна.
+
+Метод console.log() — это способ вывести отладочную информацию, не мешая пользователю.
+*/
